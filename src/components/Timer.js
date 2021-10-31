bebe54b5bee8b966529304176cd63b9b34aefd0e
rrTong <rrtong@ucdavis.edu>
2021-10-31 14:59:21 -0700

    rework timer logic, speed timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,20 +1,22 @@
 import { useState, useEffect } from "react";
 import moment from "moment";
 
-const format = "hh:mm:ss";
+// const format = "hh:mm:ss";
 const timerEnd = moment().add(2, "hours");
 
 const Timer = () => {
   const [timer, setTimer] = useState(moment());
-
-  const timeDiff = timerEnd - timer;
-  const seconds = Math.floor((timeDiff / 1000) % 60);
-  const minutes = Math.floor((timeDiff / 1000 / 60) % 60);
-  const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+  // const [showGreen, setShowGreen] = useState(false);
+  let timeDiff = moment.duration(timerEnd.diff(timer));
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimer(moment());
+      setTimer((prevTimer) => prevTimer.clone().add(1, "seconds"));
+
+      // console.log(timer.clone().format(format));
+      // if (timer.clone().isSame(timerEnd) || timer.clone().isAfter(timerEnd)) {
+      //   setShowGreen(true);
+      // }
     }, 1000);
 
     return () => clearInterval(interval);
@@ -22,9 +24,39 @@ const Timer = () => {
 
   return (
     <>
-      <span>{hours}h</span>
-      <span>{minutes}m</span>
-      <span>{seconds}s</span>
+      <div>
+        <span>{timeDiff.hours()}h</span>
+        <span>{timeDiff.minutes()}m</span>
+        <span>{timeDiff.seconds()}s</span>
+      </div>
+      <button
+        onClick={() => {
+          setTimer((prevTimer) => prevTimer.clone().add(1, "seconds"));
+        }}
+      >
+        -1 second
+      </button>
+      <button
+        onClick={() => {
+          setTimer((prevTimer) => prevTimer.clone().add(1, "minutes"));
+        }}
+      >
+        -1 minute
+      </button>
+      <button
+        onClick={() => {
+          setTimer((prevTimer) => prevTimer.clone().add(5, "minutes"));
+        }}
+      >
+        -5 minutes
+      </button>
+      {/* <div
+        style={{
+          backgroundColor: showGreen && "green",
+          height: "100px",
+          width: "100px",
+        }}
+      ></div> */}
     </>
   );
 };

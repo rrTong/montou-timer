4576cb6a3c08f37dd29f26a92f5eb0c5ba1db6cf
rrTong <rrtong@ucdavis.edu>
2021-11-03 00:29:44 -0700

    add regex for entering timer

--- src/App.js
+++ src/App.js
@@ -6,19 +6,31 @@ import "./App.css";
 function App() {
   const [timerStart, setTimerStart] = useState(moment());
   const [timerEnd, setTimerEnd] = useState(moment().add(2, "hours"));
-  const [timerInput, settimerInput] = useState("02:00:00");
+  const [timerInput, setTimerInput] = useState("02:00:00");
   let timerInputH = 2;
   let timerInputM = 0;
   let timerInputS = 0;
 
   const handleTimerInput = (e) => {
-    [timerInputH, timerInputM, timerInputS] = e.split(":");
+    const re = /^\d{0,2}:?[0-9]{0,2}:?[0-9]{0,2}$/;
+    if (e.target.value.match(re) != null) {
+      if (e.target.value.length === 2 || e.target.value.length === 5) {
+        e.target.value += ":";
+      }
+      e.target.value = e.target.value.replace(/:+/g, ":");
+    }
+
+    [timerInputH, timerInputM, timerInputS] = e.target.value.split(":");
+
+    timerInputH = timerInputH || 0;
+    timerInputM = timerInputM || 0;
+    timerInputS = timerInputS || 0;
   };
 
   return (
     <div className="App">
       <h1>Montou Timer</h1>
-      <input type="text" onChange={(e) => handleTimerInput(e.target.value)} />
+      <input type="text" onChange={(e) => handleTimerInput(e)} />
       <button
         className="button"
         onClick={() => {
